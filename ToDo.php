fe90bbdfa866ce34d1f0b130bff0793fe834eb9d
Felix Koch <fkoch@student.tgm.ac.at>
2020-05-20 17:20:55 +0200

    todo-class

---
+++ ToDo.php
@@ -0,0 +1,51 @@
+<?php
+
+
+class ToDo
+{
+    private $bezeichnung;
+    private $fach;
+    private $gemacht;
+    private $deadline;
+
+    public function __construct($bezeichnung,$fach,$deadline,$gemacht)
+    {
+        $this->bezeichnung = $bezeichnung;
+        $this->fach = $fach;
+        $this->deadline = $deadline;
+        $this->gemacht = $gemacht;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBezeichnung()
+    {
+        return $this->bezeichnung;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFach()
+    {
+        return $this->fach;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isDone()
+    {
+        return $this->gemacht;
+    }
+
+    /**
+     * ob die deadline überschritten wurde
+     */
+    public function isOverdue() {
+        timeJ
+    }
+
+
+}
